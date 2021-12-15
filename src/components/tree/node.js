7260e8e0e12daf0bd922ca3236f9557c0372da7a
Johan <140156@unsaac.edu.pe>
2021-12-15 13:23:32 -0500

    component nodo added

--- src/components/tree/node.js
+++ src/components/tree/node.js
@@ -1,15 +1,15 @@
 import React from "react"
 
-export default class NodeLabel extends React.PureComponent {
-  render() {
-    const {className, nodeData} = this.props
-    return (
-    <div className={className}>
-      <h2>{nodeData.name}</h2>
-      {nodeData._children && 
-        <button>{nodeData._collapsed ? 'Expand' : 'Collapse'}</button>
-      }
-    </div>
-    )
-  }
-}
+const Nodo = ({ nodeDatum, toggleNode}) => (
+  <g>
+    <circle cx="0" cy="10" r="10" aria-labelledby="mobileSupport" onClick={toggleNode} />
+    <text fill="black" strokeWidth="1" x="10" y="0" onClick={toggleNode} >
+      {nodeDatum.name}
+    </text>
+    <title id="mobileSupport">
+      {nodeDatum.attributes.title + "\n" + nodeDatum.attributes.creditos + " creditos\n" + nodeDatum.attributes.categoria}
+    </title>
+  </g>
+);
+
+export default Nodo;
