f92eeb9252ab14a6d77d2b9cf6899287aebb4c20
Johan <140156@unsaac.edu.pe>
2021-12-15 08:36:22 -0500

    footer component added

---
+++ src/components/footer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className='App-footer' style={styles}>
+            <p>jhn-cde |&nbsp;
+            <a
+                className="App-link"
+                href="https://github.com/jhn-cde"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+            github
+            </a>
+            </p>
+        </footer>
+    );
+}
+const styles = {
+    backgroundColor: "#282c34",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "calc(4px + 2vmin)",
+    color: "white",
+    width: "100%",
+    position: "fixed",
+    bottom: "0",
+    height: "5vh",
+}
+
+
+export default Footer;
+  
