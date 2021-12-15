1143fd4fc1f0e7005374e37308ab6fed45feee3a
Johan <140156@unsaac.edu.pe>
2021-12-15 08:37:21 -0500

    aside component added

---
+++ src/components/aside.js
@@ -0,0 +1,15 @@
+function Aside() {
+    return (
+        <aside className='App-aside' style={styles}>
+            <p>info here</p>
+        </aside>
+    );
+}
+const styles = {
+    minWidth: "100px",
+    maxWidth: "100px",
+    borderRight: "1px solid #393d45"
+}
+  
+export default Aside;
+  
