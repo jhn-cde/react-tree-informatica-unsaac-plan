78bb5a5302780a3efbb21e251e7820cc4858f4bb
Johan <140156@unsaac.edu.pe>
2021-12-15 08:37:01 -0500

    main component added

---
+++ src/components/main.js
@@ -0,0 +1,28 @@
+// componentes
+import Aside from './aside'
+import Section from './section'
+
+function Main({initialData}) {
+    return (
+        <div className='App-main' style={styles}>
+          <Aside />
+          <Section initialData={initialData}/>
+        </div>
+    );
+}
+const styles = {
+    //backgroundColor: "#060a12",
+    backgroundColor: "#ffffff",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "start",
+    fontSize: "calc(4px + 2vmin)",
+    color: "#060a12",
+    width: "auto",
+    minHeight: "85vh",
+    marginTop: "10vh"
+}
+  
+export default Main;
+  
