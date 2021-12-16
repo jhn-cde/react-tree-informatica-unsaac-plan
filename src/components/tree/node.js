deb2129f773e139f292d9db61bf800dcc9736ee7
Johan <140156@unsaac.edu.pe>
2021-12-15 23:32:39 -0500

    node className

--- src/components/tree/node.js
+++ src/components/tree/node.js
@@ -1,15 +1,19 @@
 import React from "react"
 
-const Nodo = ({ nodeDatum, toggleNode}) => (
-  <g>
-    <circle cx="0" cy="10" r="10" aria-labelledby="mobileSupport" onClick={toggleNode} />
-    <text fill="black" strokeWidth="1" x="10" y="0" onClick={toggleNode} >
-      {nodeDatum.name}
-    </text>
-    <title id="mobileSupport">
-      {nodeDatum.attributes.title + "\n" + nodeDatum.attributes.creditos + " creditos\n" + nodeDatum.attributes.categoria}
-    </title>
-  </g>
-);
+import './node.css'
+
+function Nodo ({ nodeDatum, toggleNode}) {
+  return (
+    <g>
+      <circle className={"node_"+nodeDatum.attributes.categoria} cx="0" cy="10" r="10" aria-labelledby="mobileSupport" onClick={toggleNode} />
+      <text fill="black" strokeWidth="1" x="10" y="0" onClick={toggleNode} >
+        {nodeDatum.name}
+      </text>
+      <title id="mobileSupport">
+        {nodeDatum.attributes.title + "\n" + nodeDatum.attributes.creditos + " creditos\n" + nodeDatum.attributes.categoria}
+      </title>
+    </g>
+  )
+}
 
 export default Nodo;
