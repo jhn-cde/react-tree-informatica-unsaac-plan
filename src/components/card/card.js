94cab5e86e29ba9500dc440b5032af5c878c7461
Johan <140156@unsaac.edu.pe>
2021-12-16 12:43:46 -0500

    Update className b

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -8,13 +8,13 @@ function Card({estilos}) {
             <div className="Card">
                 <p className="title">Leyenda</p>
                 <p>
-                    <b className="EG">&#9899;</b> Categoría EG<br/>
-                    <b className="OE">&#9899;</b> Categoría OE<br/>
-                    <b className="EE">&#9899;</b> Categoría EE<br/>
-                    <b className="EC">&#9899;</b> Categoría EC<br/>
-                    <b className="PP">&#9899;</b> Categoría PP<br/>
-                    <b className="CU">&#9899;</b> Cursado<br/>
-                    <b className="UN">&#9899;</b> Sin categoria
+                    <b className="circle EG">&#9679;</b> Categoría EG<br/>
+                    <b className="circle OE">&#9679;</b> Categoría OE<br/>
+                    <b className="circle EE">&#9679;</b> Categoría EE<br/>
+                    <b className="circle EC">&#9679;</b> Categoría EC<br/>
+                    <b className="circle PP">&#9679;</b> Categoría PP<br/>
+                    <b className="circle CU">&#9679;</b> Cursado<br/>
+                    <b className="circle UN">&#9679;</b> Sin categoria
                 </p>
             </div>
         </div>
